bad2e72db6c8aa69b512dae73076bb252325993e
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-10-07 00:12:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = "I don't do much.";
+var digitalClock = {
+  time:Math.round( Date.now() / 1000 ),
+  startTicking: function(){
+    // this.time;
+    setInterval(this.time++,1000);
+  }
+};
+
+function censor(word,phrase) {
+  return phrase.replace(RegExp(word, 'g'),'BLEEP');
+}
+
+var violenceCensor = censor.bind(null,'violence');
+var drugsCensor = censor.bind(null,'drugs');
